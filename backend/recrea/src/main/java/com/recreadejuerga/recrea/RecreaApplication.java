54718b54e29d4262fc44a6686c69351e684d5fb4
jodric10 <jorgemodric8@gmail.com>
2025-04-14 09:44:28 +0200

    feat: añadir estructura base del proyecto Angular

---
+++ backend/recrea/src/main/java/com/recreadejuerga/recrea/RecreaApplication.java
@@ -0,0 +1,13 @@
+package com.recreadejuerga.recrea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecreaApplication.class, args);
+	}
+
+}
---
+++ backend/recrea/src/test/java/com/recreadejuerga/recrea/RecreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recreadejuerga.recrea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
