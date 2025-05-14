6479c0d2339093a4b279dce8bf095f8dda5389f9
Jorge González Pérez <jorge.gperez@babelgroup.com>
2025-05-14 09:04:20 +0200

    feat(documentacion-validacion): añade soporte de OpenAPI y validación con Bean Validation
    Se instala y configura OpenAPI para documentar los endpoints de la API. Se integran DTOs con
    anotaciones de Bean Validation para validar las entradas. Se actualiza el controlador
    GlobalErrorControlador para manejar correctamente los errores de validación con
    handleMethodArgumentNotValid. Los controladores ahora usan @Valid en los parámetros de entrada para
    activar la validación automática de Jackson.

--- backend/recrea/src/main/java/com/recreadejuerga/recrea/error/GlobalErrorControlador.java
+++ backend/recrea/src/main/java/com/recreadejuerga/recrea/error/GlobalErrorControlador.java
@@ -1,18 +1,23 @@
 package com.recreadejuerga.recrea.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorControlador extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EquipoNoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ProblemDetail handleEquipoNoEncontrado(EquipoNoEncontradoException ex,  WebRequest request){
         ProblemDetail resultado= ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         resultado.setTitle("Equipo no encontrado");
@@ -22,6 +27,7 @@ public class GlobalErrorControlador extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(EquipoYaExistenteException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ProblemDetail handleEquipoYaExistente(EquipoYaExistenteException ex, WebRequest request){
         ProblemDetail resultado= ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
         resultado.setTitle("Equipo ya existente");
@@ -31,6 +37,7 @@ public class GlobalErrorControlador extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(CamposDuplicadosException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ProblemDetail handleRegistroYaExistente(CamposDuplicadosException ex, WebRequest request){
         ProblemDetail resultado= ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
         resultado.setTitle("Campos duplicados");
@@ -38,5 +45,36 @@ public class GlobalErrorControlador extends ResponseEntityExceptionHandler {
         resultado.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
         resultado.setProperty("campos", ex.getCampos());
         return resultado;
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        Map<String, String> errores = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fe -> {
+                            String msg = fe.getDefaultMessage();
+                            return msg != null ? msg : "Valor inválido";
+                        },
+                        (msg1, msg2) -> msg1
+                ));
+
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                status,
+                "Validación de entrada fallida"
+        );
+        problem.setTitle("Error de validación");
+        problem.setType(URI.create("https://api.recreadejuerga.com/errores/error-validacion"));
+        problem.setProperty("errors", errores);
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
     }
 }
