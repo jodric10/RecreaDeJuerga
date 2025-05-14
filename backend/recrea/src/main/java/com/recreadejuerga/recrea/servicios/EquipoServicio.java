79f00f21dfd88ab420073c5724cccaa7ba765fb7
Jorge González Pérez <jorge.gperez@babelgroup.com>
2025-05-14 08:23:46 +0200

    feat(equipo-servicio): añade soporte para DTOs y adapta EquipoServicio
    Se crean nuevos DTOs para estructurar la información de entrada y salida y se modifica EquipoServicio para
    utilizar estos DTOs en lugar de las entidades directamente. Esto mejora la separación de capas y la
    mantenibilidad del código.

--- backend/recrea/src/main/java/com/recreadejuerga/recrea/servicios/EquipoServicio.java
+++ backend/recrea/src/main/java/com/recreadejuerga/recrea/servicios/EquipoServicio.java
@@ -1,5 +1,10 @@
 package com.recreadejuerga.recrea.servicios;
 
+import com.recreadejuerga.recrea.dtos.equipo.ClasificacionCamposPrincipales;
+import com.recreadejuerga.recrea.dtos.equipo.EquipoDTO;
+import com.recreadejuerga.recrea.dtos.equipo.EquipoEditarDTO;
+import com.recreadejuerga.recrea.dtos.equipo.EquipoInsertDTO;
+import com.recreadejuerga.recrea.entidades.Clasificacion;
 import com.recreadejuerga.recrea.entidades.Equipo;
 import com.recreadejuerga.recrea.error.CamposDuplicadosException;
 import com.recreadejuerga.recrea.error.EquipoNoEncontradoException;
@@ -11,7 +16,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,22 +24,46 @@ public class EquipoServicio {
     @Autowired
     private EquipoRepositorio repo;
 
-    public List<Equipo> getEquipos(){
-        List<Equipo> resultado= repo.findAll();
-        if(resultado.isEmpty())
+    private EquipoDTO convertirAEquipoDTO(Equipo e) {
+        Clasificacion c = e.getClasificacion();
+        ClasificacionCamposPrincipales cc = null;
+        if (c != null) {
+            cc = new ClasificacionCamposPrincipales(
+                    c.getPosicion(),
+                    c.getPuntos(),
+                    c.getPartidosJugados()
+            );
+        }
+        return new EquipoDTO(
+                e.getId(),
+                e.getNombre(),
+                e.getUrl_logo(),
+                cc
+        );
+    }
+
+    public List<EquipoDTO> getEquipos(){
+        List<Equipo> equipos= repo.findAll();
+        if(equipos.isEmpty())
             throw new EquipoNoEncontradoException();
-        return resultado;
+        return equipos.stream().map(this::convertirAEquipoDTO).toList();
     }
 
-    public Equipo getEquipo(UUID id){
-        return repo.findById(id).orElseThrow(() -> new EquipoNoEncontradoException(id));
+    public EquipoDTO getEquipo(UUID id){
+        return repo.findById(id).map(this::convertirAEquipoDTO).orElseThrow(() -> new EquipoNoEncontradoException(id));
     }
 
-    public Equipo insertarEquipo(Equipo e) {
+    public EquipoDTO insertarEquipo(EquipoInsertDTO e) {
         if (repo.buscarPorNombre(e.getNombre()).isPresent())
             throw new EquipoYaExistenteException(e.getNombre());
         try {
-            return repo.save(e);
+            Equipo entidad_equipo = new Equipo();
+            entidad_equipo.setId(UUID.randomUUID());
+            entidad_equipo.setNombre(e.getNombre());
+            entidad_equipo.setUrl_logo(e.getUrl_logo());
+            Equipo equipo=repo.save(entidad_equipo);
+            return convertirAEquipoDTO(equipo);
+
         } catch (DataIntegrityViolationException ex) {
             Throwable causa = ex.getRootCause();
             if (causa != null) {
@@ -53,11 +81,11 @@ public class EquipoServicio {
     }
 
 
-    public Equipo modificarEquipo(Equipo e, UUID id){
+    public EquipoDTO modificarEquipo(EquipoEditarDTO e, UUID id){
         if(repo.existsById(id)){
             try {
                 repo.actualizarEquipo(id, e.getNombre(), e.getUrl_logo());
-                return repo.findById(id).orElseThrow(()-> new EquipoNoEncontradoException(id));
+                return repo.findById(id).map(this::convertirAEquipoDTO).orElseThrow(()-> new EquipoNoEncontradoException(id));
             }catch (DataIntegrityViolationException ex){
                 Throwable causa = ex.getRootCause();
                 if (causa != null) {
