f815719134f85b44a04d2ff3433ea81a905eb34b
jodric10 <jorgemodric8@gmail.com>
2025-06-01 21:23:27 +0200

    feat(footer): creó el componente footer

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../../services/toastMessage/toast.service';
 import { ToastMessage } from '../../models/toastMessage/toastMessage';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
